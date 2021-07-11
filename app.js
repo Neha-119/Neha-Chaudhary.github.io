9815d2026e943ac3f687069d8f04e5dd3dc7b22f
Neha Chaudhary <70510537+Neha-119@users.noreply.github.com>
2021-07-11 21:41:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ const loadingOverlay = document.querySelector('.loading-overlay');
 setTimeout(() => {
   loadingOverlay.classList.add('dissappear');
 }, 1500);
+setTimeout(function() {
+  $('#loading').addClass('hidden');
+}, 2000);
